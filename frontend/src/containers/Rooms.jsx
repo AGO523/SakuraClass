e87e309db1f136d1cddee44dcc7e0b5fa06dd314
AGO523 <unpouletfrais0523@gmail.com>
2021-07-06 05:46:41 +0900

    rails-reactの接続

--- frontend/src/containers/Rooms.jsx
+++ frontend/src/containers/Rooms.jsx
@@ -1,16 +1,18 @@
-import React, { Fragment } from 'react';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
+import React, { Fragment, useEffect } from 'react';
+// import Button from '@material-ui/core/Button';
+// import Container from '@material-ui/core/Container';
+import { fetchRooms } from '../apis/rooms';
 
 export const Rooms = () => {
+  useEffect(() => {
+    fetchRooms()
+      .then((data) =>
+        console.log(data)
+      )
+  }, [])
   return (
     <Fragment>
-      <Container>
-        top page
-        <Button variant="contained" color="primary">
-          Test
-        </Button>
-      </Container>
+      top page
     </Fragment>
   )
 }
