a1eb3af9d673c2849b1c46e994c5b4923d6c9a4d
AGO523 <unpouletfrais0523@gmail.com>
2021-07-07 05:59:42 +0900

    styled-components

--- frontend/src/containers/Rooms.jsx
+++ frontend/src/containers/Rooms.jsx
@@ -1,10 +1,17 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 import styled from 'styled-components';
 // import Button from '@material-ui/core/Button';
 // import Container from '@material-ui/core/Container';
 
 //apis
 import { fetchRooms } from '../apis/rooms';
+
+//reducers
+import {
+  initialState,
+  roomsActionTyps,
+  roomsReducer,
+} from '../reducers/rooms';
 
 //images
 import MainLogo from '../images/logo.png';
@@ -13,11 +20,11 @@ import MainCoverImage from '../images/main-cover-image.png';
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: flex-start;
-  padding: 8px 32px;
+  padding: 4px 8px;
 `;
 
 const MainLogoImage = styled.img`
-  height: 90px;
+  height: 40px;
 `
 
 const MainCoverImageWrapper = styled.div`
@@ -25,14 +32,21 @@ const MainCoverImageWrapper = styled.div`
 `;
 
 const MainCover = styled.img`
-  height: 600px;
+  height: 450px;
 `;
 
 export const Rooms = () => {
+  const [state, dispatch] = useReducer(roomsReducer, initialState);
   useEffect(() => {
+    dispatch({ type: roomsActionTyps.FETCHING });
     fetchRooms()
       .then((data) =>
-        console.log(data)
+        dispatch({
+          type: roomsActionTyps.FETCH_SUCCESS,
+          payload: {
+            rooms: data.rooms
+          }
+        })
       )
   }, [])
   return (
@@ -43,6 +57,13 @@ export const Rooms = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
+      {
+        state.roomsList.map(room =>
+          <div>
+            {room.name}
+          </div>
+        )
+      }
     </Fragment>
   )
 }
