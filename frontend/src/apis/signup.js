1c2c61568aa4aa064c96ab5ef40f74841ec76506
AGO523 <unpouletfrais0523@gmail.com>
2021-07-25 14:32:07 +0900

    パラメーターの関数を定義中

--- frontend/src/apis/signup.js
+++ frontend/src/apis/signup.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import { signup } from '../urls/index'
+import client from "../api/client"
+import Cookies from "js-cookie"
+
+import { SignUpParams, SignInParams } from "interfaces/index"
 
 // サインアップ（新規アカウント作成）
 export const fetchSignup = (params) => {
